67b11663072745238170f9e11db224ec17f3721b
rama <rama@RAMA.HIREZON.local>
2020-01-03 17:02:47 -0500

    Singleton Design Pattern

---
+++ Singleton/src/Program.java
@@ -0,0 +1,26 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+				Singleton x = Singleton.getInstance(); 
+
+				Singleton y = Singleton.getInstance(); 
+
+				Singleton z = Singleton.getInstance(); 
+
+				x.s = (x.s).toUpperCase(); 
+
+				System.out.println("String from x is " + x.s); 
+				System.out.println("String from y is " + y.s); 
+				System.out.println("String from z is " + z.s); 
+				System.out.println("\n"); 
+
+				z.s = (z.s).toLowerCase(); 
+
+				System.out.println("String from x is " + x.s); 
+				System.out.println("String from y is " + y.s); 
+				System.out.println("String from z is " + z.s); 
+	}
+
+}
